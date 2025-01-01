dff0b6e89d4d8d28641f8c0db8820d2d780accea
Nez <leenezzy99@gmail.com>
2025-01-01 20:24:04 +0200

    Initial commit of created classes

---
+++ Client.java
@@ -0,0 +1,81 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clientId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "advisorID", nullable = false)
+    private Advisor advisor;
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
+    private Portfolio portfolio;
+
+    public Client(String firstName, String lastName, String phoneNumber, Advisor advisor ){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Advisor getAdvisor() {
+        return advisor;
+    }
+
+    public void setAdvisor(Advisor advisor) {
+        this.advisor = advisor;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+}
---
+++ Security.java
@@ -0,0 +1,88 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.List;
+@Entity
+public class Security {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long securityId;
+
+    @ManyToOne
+    @JoinColumn(name = "portfolioId", nullable = false)
+    private Portfolio portfolio;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date purchaseDate;
+
+    @Column(nullable = false)
+    private double purchasePrice;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    public Security(Long securityId, Portfolio portfolio, String name, String category, Date purchaseDate, double purchasePrice, int quantity) {
+        this.portfolio = portfolio;
+        this.name = name;
+        this.category = category;
+        this.purchaseDate = purchaseDate;
+        this.purchasePrice = purchasePrice;
+        this.quantity = quantity;
+    }
+
+    public Long getSecurityId(){
+        return securityId;
+    }
+
+    public Portfolio getPortfolio(){
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio){
+        this.portfolio = portfolio;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
